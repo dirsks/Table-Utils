d8f428540a1668b19acba074ae0391ca4c435fd0
366218094fe6aa <sayminho383@gmail.com>
2026-07-25 19:39:36 -0300

    Bump version to 1.2.0 and enhance TableUtils
    Updated version numbers and added new methods for table management.

--- Main.lua
+++ Main.lua
@@ -3,9 +3,9 @@
 							Table Utils
 						 Read Before Using
 	
-	Version: 1.1.2
+	Version: 1.2.0
 	
-	Release Version: 1.1.2
+	Release Version: 1.2.0
 
 	TODO: 
 	TableUtils.new() --> This is the normal Table. Examples:
@@ -100,6 +100,22 @@
 	
 	--INFO: This function isn't recommended for casual uses
 	Table:_run() --> A internal functon that can be used for :proccess method.
+	
+	Table:clear() --> Destroy every existing element on the table. This function also can return true or false when realizing the action.
+	`Table:push('Banana')
+	 Table:push('Banana 2')
+	 Table:push('Apple ')
+	 Table:push('Apple')
+	 Table:push('Apple 1')
+	 
+	 Table:clear() --> everything will be destroyed
+	 -- OR --
+	 if Table:clear() then
+	 	print('Successfully cleared Table!')
+	 else
+	 	print('Something went wrong!')
+	 end
+	`
 ]]
 local TableUtils={};
 TableUtils.__index = TableUtils
@@ -110,10 +126,19 @@ function TableUtils.new()
 	self._last=0;
 	self._listeners={};
 	self._isProcessing=false;
-	self._handler=nil
+	self._handler=nil;
+	self._destroyed=false;
 	return self;
 end;
 function TableUtils:push(value)
+	if self:isDestroyed() then
+		warn('TableUtils:push() ignored: the table is already destroyed (:destroy())');
+		return;
+	end;
+	if value==nil then
+		warn('TableUtils:push() ignored: attempted to add a nil value to the table');
+		return;
+	end;
 	self._last = self._last + 1;
 	self._list[self._last]=value;
 	self:_notify(value);
@@ -129,6 +154,10 @@ function TableUtils:pop()
 	return value;
 end;
 function TableUtils:removeAt(position)
+	if type(position)~='number' then
+		warn('TableUtils:removeAt() waits a number as index, but received:', tostring(position));
+		return nil;
+	end;
 	local internalIndex=self._first+position-1;
 	if internalIndex<self._first or internalIndex>self._last then
 		return nil;
@@ -142,6 +171,9 @@ function TableUtils:removeAt(position)
 	return removedValue;
 end
 function TableUtils:remove(targetValue)
+	if targetValue==nil then
+		return nil;
+	end;
 	for i=self._first,self._last do
 		if self._list[i]==targetValue then
 			local relativePosition=(i-self._first)+1;
@@ -150,6 +182,13 @@ function TableUtils:remove(targetValue)
 	end;
 	return nil;
 end;
+function TableUtils:clear()
+	self:stopProcess();
+	self._list={};
+	self._first=1;
+	self._last=0;
+	return true;
+end;
 function TableUtils:isEmpty()
 	return self._first>self._last;
 end;
@@ -157,13 +196,18 @@ function TableUtils:size()
 	return (self._last-self._first)+1;
 end;
 function TableUtils:onPush(callback)
-	if type(callback) == "function" then
-		table.insert(self._listeners,callback);
+	if type(callback) ~= 'function' then
+		warn('TableUtils:onPush() waits a function as argument, received:', tostring(callback));
+		return;
 	end;
+	table.insert(self._listeners,callback);
 end;
 function TableUtils:_notify(item)
 	for _,listener in ipairs(self._listeners) do
-		pcall(listener,item,self:size());
+		local success,err=pcall(listener,item,self:size());
+		if not success then
+			warn('Error ocurred on a listener while running onPush:', err);
+		end;
 	end;
 end;
 function TableUtils:peek()
@@ -171,12 +215,12 @@ function TableUtils:peek()
 	return self._list[self._first]
 end
 function TableUtils:_run()
-	if self._isProcessing or not self._handler or self:isEmpty() then 
+	if self._isProcessing or not self._handler or self:isEmpty() or self:isDestroyed() then 
 		return 
 	end
 	self._isProcessing=true
 	task.spawn(function()
-		while not self:isEmpty() and self._isProcessing do
+		while not self:isEmpty() and self._isProcessing and not self:isDestroyed() do
 			local currentIndex=self._first
 			local currentItem=self:peek()
 			local success,result=pcall(self._handler,currentItem,currentIndex)
@@ -184,7 +228,7 @@ function TableUtils:_run()
 				self:pop()
 			else
 				if not success then
-					warn('Cannot proccess item:', result)
+					print('Cannot proccess item:', result)
 				end
 				task.wait() 
 			end
@@ -199,20 +243,47 @@ function TableUtils:stopProcess()
 end
 
 function TableUtils:process(handlerFunc)
-	--if self._isProcessing then return end
-	--self._isProcessing=true;
-	--while self._isProcessing and not self:isEmpty() do
-	--	local currentIndex=self._first;
-	--	local currentItem=self:pop();
-	--	local success,result=pcall(handlerFunc,currentItem,currentIndex);
-	--	if not success or result~=true then
-	--		self._isProcessing=false;
-	--		break;
-	--	end;
-	--end;
-	--self._isProcessing=false;
+	if type(handlerFunc) ~= "function" then
+		print('TableUtils:process() waits a function as argument, received:', tostring(handlerFunc));
+		return;
+	end;
 	self._handler = handlerFunc
 	self:_run()
 end
-return TableUtils;
 
+function TableUtils:loopWhileExists(callback, interval)
+	if type(callback) ~= "function" then
+		print('TableUtils:loopWhileExists() waits a function as argument, received:', tostring(callback));
+		return;
+	end;
+	interval = interval or 0;
+	if type(interval) ~= "number" or interval < 0 then
+		print('TableUtils:loopWhileExists() received an invalid interval, using 0');
+		interval = 0;
+	end;
+	task.spawn(function()
+		while not self:isDestroyed() do
+			local success,err=pcall(callback,self);
+			if not success then
+				warn('Error while runningloopWhileExists:', err);
+			end;
+			task.wait(interval);
+		end;
+	end);
+end
+
+function TableUtils:isDestroyed()
+	return self._destroyed==true;
+end
+
+function TableUtils:destroy()
+	if self:isDestroyed() then
+		return;
+	end;
+	self:clear();
+	self._listeners={};
+	self._handler=nil;
+	self._destroyed=true;
+end
+
+return TableUtils;
